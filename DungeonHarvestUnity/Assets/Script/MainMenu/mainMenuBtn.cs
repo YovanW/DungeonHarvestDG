6c25411bd645576d4089095e7d304d487a65b029
Kyrozu <nat80222@gmail.com>
2025-11-12 13:17:35 +0700

    MainMenu Canvas

---
+++ DungeonHarvestUnity/Assets/Script/MainMenu/mainMenuBtn.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class mainMenuBtn : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
