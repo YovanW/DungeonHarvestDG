72d3be1b95f4455ec26b182742d40c0b1418c287
Kyrozu <nat80222@gmail.com>
2025-11-19 14:15:55 +0700

    Main Menu + Setting

---
+++ DungeonHarvestUnity/Assets/Script/MainMenu/MenuBtn.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MenuBtn : MonoBehaviour
+{
+    public GameObject mainCanvas;
+    public GameObject playCanvas;
+    public GameObject settingCanvas;
+
+    public Button playBtn;
+    public Button settingBtn;
+    public Button newGameBtn;
+
+    public Button continueBtn;
+    public Button normalModeBtn;
+    public Button hardModeBtn;
+    public Button backBtn;
+    public Button settingBackBtn;
+
+    private string savedData;
+
+    void Start()
+    {
+        // Assign button listeners
+        playBtn.onClick.AddListener(play);
+        settingBtn.onClick.AddListener(setting);
+        continueBtn.onClick.AddListener(continueGame);
+        newGameBtn.onClick.AddListener(newGame);
+        backBtn.onClick.AddListener(back);
+        settingBackBtn.onClick.AddListener(back);
+
+
+        // Disable Continue button if no save data
+        if (!PlayerPrefs.HasKey("SavedGame"))
+        {
+            continueBtn.interactable = false;
+        }
+
+        // //FIXME: test change scene
+        // continueBtn.interactable = true;
+
+        // Hide other canvases
+        playCanvas.SetActive(false);
+        settingCanvas.SetActive(false);
+
+        // Hide mode buttons at start
+        normalModeBtn.gameObject.SetActive(false);
+        hardModeBtn.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Esc key to go back to main menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (playCanvas.activeSelf || settingCanvas.activeSelf)
+            {
+                back();
+            }
+        }
+    }
+
+    // Back to Main Menu
+    public void back()
+    {
+        playCanvas.SetActive(false);
+        settingCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
+    }
+
+    // Main Menu
+    public void play()
+    {
+        mainCanvas.SetActive(false);
+        playCanvas.SetActive(true);
+    }
+
+    public void setting()
+    {
+        mainCanvas.SetActive(false);
+        settingCanvas.SetActive(true);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+
+    // Play Menu
+    public void continueGame()
+    {
+        Debug.Log("Continue Game");
+
+        //TODO: Load saved game data
+        if (PlayerPrefs.HasKey("SavedGame"))
+        {
+            savedData = PlayerPrefs.GetString("SavedGame");
+
+            // // Load the saved game scene
+            // SceneManager.LoadScene("Game");
+        }
+
+        // FIXME: test scene load
+        SceneManager.LoadScene("Game");
+    }
+
+    public void newGame()
+    {
+        bool isActive = normalModeBtn.gameObject.activeSelf;
+
+        // toggle both buttons
+        normalModeBtn.gameObject.SetActive(!isActive);
+        hardModeBtn.gameObject.SetActive(!isActive);
+
+        // force the button to return to normal state
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void normalMode()
+    {
+        Debug.Log("Normal Mode Selected");
+        //TODO: create new game in normal mode
+
+    }
+
+    public void hardMode()
+    {
+        Debug.Log("Hard Mode Selected");
+        //TODO: create new game in hard mode
+
+    }
+
+}
---
+++ DungeonHarvestUnity/Assets/Script/MainMenu/setting.cs
@@ -0,0 +1,124 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class settingManager : MonoBehaviour
+{
+    // Display Mode Buttons
+    public Button fullscreenBtn;
+    public Button windowedBtn;
+    public Button borderlessBtn;
+    private List<Button> displayButtons;
+
+    // Gameplay settings
+    [SerializeField] private TextMeshProUGUI mouseValueText;
+    [SerializeField] private Slider mouseSlider;
+
+    // Audio settings
+    /*  AUDIO MIXER SETUP
+
+        1. Buat Audio Mixer
+           - Project → Create → Audio → Audio Mixer
+           - Contoh nama: GameAudio
+
+        2. Buat Mixer Group
+           Di dalam mixer, buat tiga group:
+             • Master
+             • Music
+             • VFX
+
+           Struktur akhirnya:
+             Master
+             ├── Music
+             └── VFX
+
+        3. Pasang Mixer Group ke AudioSource
+           - Pilih GameObject yang punya AudioSource
+           - Pada AudioSource → Output pilih:
+               • BGM / lagu          → Music
+               • Semua sound effect  → VFX
+                 (footstep, UI click, hit, explosion, magic, wind, dll)
+
+           Catatan:
+           - Semua SFX masuk ke VFX biar gampang dikontrol satu slider.
+
+        4. Expose Volume Parameters
+           - Di Audio Mixer, klik tombol knob Volume pada tiap group
+           - Klik kanan → Expose Volume
+           - Ganti nama parameter jadi:
+               • MasterVol
+               • MusicVol
+               • VFXVol
+
+        5. Hubungkan Slider ke Audio Mixer (di script)
+           - Gunakan mixer.SetFloat("MusicVol", valueDalamDb);
+           - Konversi slider (0–1) ke dB memakai:
+               Mathf.Log10(sliderValue) * 20
+
+        6. Nilai umum:
+           - Volume normal = 0 dB
+           - Mute          = -80 dB
+    */
+    public AudioMixer audioMixer = null; // belum dibuat
+
+    [SerializeField] private TextMeshProUGUI masterValueText;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private TextMeshProUGUI musicValueText;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private TextMeshProUGUI vfxValueText;
+    [SerializeField] private Slider vfxSlider;
+
+    void Start()
+    {
+        displayButtons = new List<Button> { fullscreenBtn, windowedBtn, borderlessBtn };
+        fullscreenBtn.onClick.AddListener(() => SelectDisplayMode(fullscreenBtn));
+        windowedBtn.onClick.AddListener(() => SelectDisplayMode(windowedBtn));
+        borderlessBtn.onClick.AddListener(() => SelectDisplayMode(borderlessBtn));
+
+        SelectDisplayMode(fullscreenBtn);   // default display mode
+
+        mouseSlider.onValueChanged.AddListener(SetMouseSensitivity);
+
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        vfxSlider.onValueChanged.AddListener(SetVFXVolume);
+    }
+
+    void SelectDisplayMode(Button selected)
+    {
+        foreach (var btn in displayButtons)
+            btn.interactable = true;
+
+        selected.interactable = false;
+        Debug.Log("Selected Display Mode: " + selected.name);
+    }
+
+    public void SetMouseSensitivity(float value)
+    {
+        // TODO: Apply mouse sensitivity setting to the game
+        mouseValueText.text = value.ToString("F2");
+
+    }
+
+
+    public void SetMasterVolume(float value)
+    {
+        // audioMixer.SetFloat("MasterVol", Mathf.Lerp(-80, 0, value));
+        masterValueText.text = Mathf.RoundToInt(value).ToString();
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        // audioMixer.SetFloat("MusicVol", Mathf.Lerp(-80, 0, value));
+        musicValueText.text = Mathf.RoundToInt(value).ToString();
+    }
+
+    public void SetVFXVolume(float value)
+    {
+        // audioMixer.SetFloat("VFXVol", Mathf.Lerp(-80, 0, value));
+        vfxValueText.text = Mathf.RoundToInt(value).ToString();
+    }
+
+}
