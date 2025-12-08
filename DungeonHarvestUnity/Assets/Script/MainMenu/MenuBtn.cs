5f928bc9c6a3df2b97c653af2a560b64f497fcc3
Kyrozu <nat80222@gmail.com>
2025-12-08 22:51:59 +0700

    Demo1

--- DungeonHarvestUnity/Assets/Script/MainMenu/MenuBtn.cs
+++ DungeonHarvestUnity/Assets/Script/MainMenu/MenuBtn.cs
@@ -106,7 +106,8 @@ public class MenuBtn : MonoBehaviour
         }
 
         // FIXME: test scene load
-        SceneManager.LoadScene("nathanTest");
+        // SceneManager.LoadScene("nathanTest");
+        SceneManager.LoadScene("Demo");
     }
 
     public void newGame()
